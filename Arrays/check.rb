62cc28bca4556e3c0201d3cadca4f2351a116736
Robert C. Gouge <robert.c.gouge@gmail.com>
2019-06-18 19:21:41 -0500

    Arrays exercises

---
+++ Arrays/check.rb
@@ -0,0 +1,6 @@
+arr = [1, 3, 5, 7, 9, 11]
+number = 3
+
+if arr.select {|i| i == number}
+    puts "#{number} is in array"
+end
---
+++ Arrays/new_array.rb
@@ -0,0 +1,9 @@
+arr = [1,2,3,4,5,6,7,8,9]
+second_arr = []
+
+arr.each do |i|
+  second_arr << i *2
+end
+
+p arr
+p second_arr
